3ecffbdc86464c93429c8c5cc33aa658240589d0
Ibrahim Kalin <hisetkal@gmail.com>
2021-09-30 18:42:42 -0400

    third updates

--- src/package1/ClassOne.java
+++ src/package1/ClassOne.java
@@ -6,7 +6,11 @@ public class ClassOne {
 	public static void main(String[] args) {
 		System.out.println("Hello Everyone");
 		System.out.println("Hi from local Ibrahim");
+
 		System.out.println("Hello Ibrahim");
+
+		System.out.println("Hello Master");
+
 	}
 
 }
