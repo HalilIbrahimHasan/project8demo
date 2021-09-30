ac58afeae9e562235ecf2fe0d79f8d7ae22e34ec
Ibrahim Kalin <hisetkal@gmail.com>
2021-09-30 18:36:22 -0400

    second commit

--- src/package1/ClassOne.java
+++ src/package1/ClassOne.java
@@ -6,6 +6,7 @@ public class ClassOne {
 	public static void main(String[] args) {
 		System.out.println("Hello Everyone");
 		System.out.println("Hi from local Ibrahim");
+		System.out.println("Hello Ibrahim");
 	}
 
 }
